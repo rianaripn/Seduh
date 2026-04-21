325bbee27e8b2d96c483eaf1c4fec1b071ea4269
Rian Arip Nugraha <rianarip4@gmail.com>
2026-04-22 01:15:20 +0800

    add render req

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const sqlite3 = require('sqlite3').verbose();
 const PDFDocument = require('pdfkit');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // ==========================================
 // 1. MIDDLEWARE 
